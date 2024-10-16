01e627a2d67b49d4ec45e5b69a2dd33ccbd580e5
Shirayu <tom.shadow14@gmail.com>
2024-10-16 09:45:17 +0200

    graphic palette added

--- nodeJsServer/tailwind.config.ts
+++ nodeJsServer/tailwind.config.ts
@@ -9,9 +9,25 @@ const config: Config = {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        'text': '#09101B',
+        'background': '#F7FAFC',
+        'primary': '#2C79DD',
+        'secondary': '#81B3F3',
+        'accent': '#4C98FA',
+        'gray': {
+          'light': "#E2E8F0",
+          "dark": "#A0AEC0"
+        }
       },
+      fontSize: {
+        sm: '0.750rem',
+        base: '1rem',
+        xl: '1.333rem',
+        '2xl': '1.777rem',
+        '3xl': '2.369rem',
+        '4xl': '3.158rem',
+        '5xl': '4.210rem',
+      }
     },
   },
   plugins: [],
