a81204c0750e20bca0a4641c396f9c870f49cd97
Shirayu <tom.shadow14@gmail.com>
2024-11-27 11:36:58 +0100

    Rollback chart history display change

--- nodeJsServer/app/dashboard/(routes)/chat/page.tsx
+++ nodeJsServer/app/dashboard/(routes)/chat/page.tsx
@@ -13,40 +13,15 @@ export default async function ChatPage() {
                 <div className="w-[440px] flex flex-col items-center gap-[10px]">
                     <Chatbot />
                 </div>
-                <div className="flex-grow rounded-[15px] flex items-center gap-[24px] px-[38px] py-[24px] bg-white shadow-[0px_3.5px_5.5px_0px_rgba(0,_0,_0,_0.02)]">
-                    <div className="flex-grow h-full flex flex-col gap-[10px]">
-                        <h1 className="text-3xl font-bold text-primary block w-full">Data visualization bot</h1>
-                        <ChartDisplay className="w-full flex-grow flex justify-center" />
-                    </div>
-                    <div className="w-[1px] h-full bg-gray-light"></div>
-                    <div className="w-[200px] h-full flex flex-col gap-[10px]">
-                        <h2 className="text-xl font-bold text-primary block w-full">Charts history</h2>
-                        <ChartHistory />
-                    </div>
+                <div className="flex-grow rounded-[15px] flex flex-col items-center gap-[10px] px-[38px] py-[24px] bg-white shadow-[0px_3.5px_5.5px_0px_rgba(0,_0,_0,_0.02)]">
+                    <h1 className="text-3xl font-bold text-primary block w-full">Data visualization bot</h1>
+                    <div className="w-full h-[1px] bg-gray-light"></div>
+                    <ChartDisplay className="w-full flex-grow flex justify-center" />
+                    <div className="w-full h-[1px] bg-gray-light"></div>
+                    <h2 className="text-xl font-bold text-primary block w-full">Charts history</h2>
+                    <ChartHistory />
                 </div>
             </div>
         </WebSocketProvider>
     );
 }
-
-// export default async function ChatPage() {
-//     const session = await getSession();
-//     console.log(session);
-//     return (
-//         <WebSocketProvider>
-//             <div className="w-full flex-grow flex justify-center gap-[30px] px-[20px] py-[50px]">
-//                 <div className="w-[440px] flex flex-col items-center gap-[10px]">
-//                     <Chatbot />
-//                 </div>
-//                 <div className="flex-grow rounded-[15px] flex flex-col items-center gap-[10px] px-[38px] py-[24px] bg-white shadow-[0px_3.5px_5.5px_0px_rgba(0,_0,_0,_0.02)]">
-//                     <h1 className="text-3xl font-bold text-primary block w-full">Data visualization bot</h1>
-//                     <div className="w-full h-[1px] bg-gray-light"></div>
-//                     <ChartDisplay className="w-full flex-grow flex justify-center" />
-//                     <div className="w-full h-[1px] bg-gray-light"></div>
-//                     <h2 className="text-xl font-bold text-primary block w-full">Charts history</h2>
-//                     <ChartHistory />
-//                 </div>
-//             </div>
-//         </WebSocketProvider>
-//     );
-// }
--- nodeJsServer/components/chart-history.tsx
+++ nodeJsServer/components/chart-history.tsx
@@ -5,14 +5,14 @@ import { cn } from '@/lib/utils';
 
 const ChartHistory = () => {
     const { currentChart, charts, setChartFromHistory } = useContext(WebSocketContext);
-    return <div className="w-[200px] h-full flex flex-col items-center relative">
-        <div className="absolute top-0 left-0 px-[10px] py-[10px] w-full h-full flex flex-col items-center gap-[30px] overflow-y-auto">
+    return <div className="w-full h-[150px] flex items-center relative">
+        <div className="absolute top-0 left-0 px-[10px] py-[10px] w-full h-full flex items-center gap-[30px] overflow-x-auto">
             {charts.filter(c => c.image).map((chart, index) => (
                 <img
                     key={index}
                     src={chart.image}
                     alt={`Chart ${index}`}
-                    className={cn("h-auto w-full object-contain rounded-[4px]", chart === currentChart && "shadow-[0px_0px_6px_0px_#4C98FA]")}
+                    className={cn("h-full w-auto object-contain rounded-[4px]", chart === currentChart && "shadow-[0px_0px_6px_0px_#4C98FA]")}
                     onClick={() => setChartFromHistory(chart)}
                 />
             ))}
@@ -22,25 +22,3 @@ const ChartHistory = () => {
 };
 
 export default ChartHistory;
-
-
-//
-// const ChartHistory = () => {
-//     const { currentChart, charts, setChartFromHistory } = useContext(WebSocketContext);
-//     return <div className="w-full h-[150px] flex items-center relative">
-//         <div className="absolute top-0 left-0 px-[10px] py-[10px] w-full h-full flex items-center gap-[30px] overflow-x-auto">
-//             {charts.filter(c => c.image).map((chart, index) => (
-//                 <img
-//                     key={index}
-//                     src={chart.image}
-//                     alt={`Chart ${index}`}
-//                     className={cn("h-full w-auto object-contain rounded-[4px]", chart === currentChart && "shadow-[0px_0px_6px_0px_#4C98FA]")}
-//                     onClick={() => setChartFromHistory(chart)}
-//                 />
-//             ))}
-//         </div>
-
-//     </div>
-// };
-
-// export default ChartHistory;
